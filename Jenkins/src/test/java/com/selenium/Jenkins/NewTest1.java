93aff2264f0996c657316ed83220487c2c50cd10
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-09-05 14:48:10 +0530

    c1

---
+++ Jenkins/src/test/java/com/selenium/Jenkins/NewTest1.java
@@ -0,0 +1,14 @@
+package com.selenium.Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("https://google.com");
+	  
+  }
+}
